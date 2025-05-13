ad2bddd45ddeeb87a021caaa4314565e4b1449c7
Иван Стародубцев <starivan2005@gmail.com>
2025-05-14 05:42:35 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 899,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 799,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 1299,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 649,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Юваль Ной Харари',
+        name: 'Sapiens. Краткая история человечества',
+        publicationYear: 2011,
+        price: 999,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Ася Казанцева',
+        name: 'Мозг материален',
+        publicationYear: 2019,
+        price: 749,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Тара Вестовер',
+        name: 'Учёная',
+        publicationYear: 2018,
+        price: 1199,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 599,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Николай Кукушкин',
+        name: 'Клетка, которая взбесилась',
+        publicationYear: 2021,
+        price: 1099,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Айзек Азимов',
+        name: 'Основание',
+        publicationYear: 1951,
+        price: 889,
+        bestseller: false,
+        available: true
+    }
+]
